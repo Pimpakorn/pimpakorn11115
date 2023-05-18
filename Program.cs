3dca3153a335731171b8ea7aeea474f96e7bf429
Pimpakorn <122685124+Pimpakorn@users.noreply.github.com>
2023-05-18 20:14:08 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool validInput = false;
+
+        while (!validInput)
+        {
+            Console.Write("กรุณากรอกอักขระ: ");
+            string input = Console.ReadLine();
+
+            if (input == "+" || input == "-")
+            {
+                validInput = true;
+                Console.Write("กรุณากรอกขนาดของเมทริกซ์: ");
+                int size = int.Parse(Console.ReadLine());
+
+                Console.Write("กรุณากรอกสมาชิกของเมทริกซ์: ");
+                double element = double.Parse(Console.ReadLine());
+
+                double result = 0;
+
+                if (input == "+")
+                    result = size + element;
+                else if (input == "-")
+                    result = size - element;
+
+                Console.WriteLine("ผลลัพธ์: " + result);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("โปรแกรมจบการทำงาน");
+    }
+}
